6cea2a945b3517c66e3667d24e20cae9c89c9821
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-11-05 19:00:20 -0300

    refactor: remove local storage

--- src/storage.ts
+++
@@ -1,8 +0,0 @@
-import { User } from './dtos/models/user';
-
-export class Storage {
-  static users: User[] = [];
-
-  static getUsers() { return Storage.users };
-  static setUsers(users: User[]) { Storage.users = users };
-}
